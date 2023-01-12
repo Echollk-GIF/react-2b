4617097a9cbebbe68c36b120e20b1a516b701b4c
Frank <841290355@qq.com>
2023-01-12 15:12:59 +0800

    feat: Layout布局

--- src/components/SandBox/TopHeader.js
+++ src/components/SandBox/TopHeader.js
@@ -1,7 +1,26 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Layout, theme } from 'antd'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from '@ant-design/icons'
+const { Header } = Layout
 export default function TopHeader () {
+  const [collapsed, setCollapsed] = useState(false)
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken()
   return (
-    <div>TopHeader</div>
+    <Header
+      style={{
+        padding: 0,
+        background: colorBgContainer,
+      }}
+    >
+      {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+        className: 'trigger',
+        onClick: () => setCollapsed(!collapsed),
+      })}
+    </Header>
   )
 }
--- src/views/SandBox/SandBox.js
+++ src/views/SandBox/SandBox.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
+
 import TopHeader from '../../components/SandBox/TopHeader'
 import SideMenu from '../../components/SandBox/SideMenu'
 import Home from './home/Home'
@@ -7,20 +8,39 @@ import UserList from './user-manage/UserList'
 import RoleList from './user-manage/right-manage/role/RoleList'
 import RightList from './user-manage/right-manage/right/RightList'
 import NoPermission from './noPermission/NoPermission'
-export default function SandBox () {
-  return (
-    <div>
-      <TopHeader />
-      <SideMenu />
 
-      <Routes>
-        <Route path="home" element={<Home />} />
-        <Route path="user-manage/list" element={<UserList />} />
-        <Route path="right-manage/role/list" element={<RoleList />} />
-        <Route path="right-manage/right/list" element={<RightList />} />
-        <Route path="/" element={<Navigate replace from="/" to="home" />} />
-        <Route path="/*" element={<NoPermission />} />
-      </Routes>
-    </div>
+import './SandBox.css'
+
+import { Layout, theme } from 'antd'
+const { Content } = Layout
+export default function SandBox () {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken()
+  return (
+    <Layout>
+      <SideMenu />
+      <Layout className="site-layout">
+        <TopHeader />
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          <Routes>
+            <Route path="home" element={<Home />} />
+            <Route path="user-manage/list" element={<UserList />} />
+            <Route path="right-manage/role/list" element={<RoleList />} />
+            <Route path="right-manage/right/list" element={<RightList />} />
+            <Route path="/" element={<Navigate replace from="/" to="home" />} />
+            <Route path="/*" element={<NoPermission />} />
+          </Routes>
+        </Content>
+      </Layout>
+
+    </Layout>
   )
 }
