b51c59828e43d2af7041f85d9fe6bf6bd6e0b210
Frank <841290355@qq.com>
2023-01-16 23:59:09 +0800

    feat: Login

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,7 +1,67 @@
 import React from 'react'
-
+import { LockOutlined, UserOutlined } from '@ant-design/icons'
+import { Button, Checkbox, Form, Input } from 'antd'
+import { useNavigate } from 'react-router-dom'
 export default function Login () {
+  const navigate = useNavigate()
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values)
+  }
   return (
-    <div>Login</div>
+    <div style={{ height: '100vh' }}>
+      <div className='formContainer'>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="用户名"
+            rules={[
+              {
+                required: true,
+                message: '请输入用户名!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="密码"
+            rules={[
+              {
+                required: true,
+                message: '请输入密码!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>记住我</Checkbox>
+            </Form.Item>
+
+            <a className="login-form-forgot" href="http://www.baidu.com">
+              忘记密码
+            </a>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              登录
+            </Button>
+            Or <Button type='primary' onClick={() => { navigate('/login') }}>现在注册!</Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   )
 }
